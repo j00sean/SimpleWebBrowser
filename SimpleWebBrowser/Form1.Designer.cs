689abeb4e8d60095e8d23ee8da83ea10ff52eda7
j00sean <j00sean.disclosures@gmail.com>
2018-07-11 19:35:00 -0700

    Upload local repository

---
+++ SimpleWebBrowser/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace SimpleWebBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wb = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.path = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // wb
+            // 
+            this.wb.Location = new System.Drawing.Point(21, 26);
+            this.wb.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wb.Name = "wb";
+            this.wb.ScriptErrorsSuppressed = true;
+            this.wb.Size = new System.Drawing.Size(755, 367);
+            this.wb.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkRed;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(692, 412);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // path
+            // 
+            this.path.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.path.Location = new System.Drawing.Point(21, 412);
+            this.path.Name = "path";
+            this.path.Size = new System.Drawing.Size(616, 26);
+            this.path.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.path);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.wb);
+            this.Name = "Form1";
+            this.Text = "Simple WebBrowser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser wb;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox path;
+    }
+}
+
---
+++ SimpleWebBrowser/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SimpleWebBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            path.Text = "http://127.0.0.1:8080/test.html";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            wb.Navigate(new Uri(path.Text));
+        }
+
+    }
+}

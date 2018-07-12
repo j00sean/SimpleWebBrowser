7e7b85d79d148903e7f32efe5016e81884818d7f
j00sean <j00sean.disclosures@gmail.com>
2018-07-12 09:40:52 -0700

    Timer to reload

--- SimpleWebBrowser/Form1.cs
+++ SimpleWebBrowser/Form1.cs
@@ -1,19 +1,35 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.Timers;
 
 namespace SimpleWebBrowser
 {
     public partial class Form1 : Form
     {
+        private System.Timers.Timer gTimer = new System.Timers.Timer();
         public Form1()
         {
             InitializeComponent();
             path.Text = "http://127.0.0.1:8080/test.html";
+            gTimer.Interval = 1000;
+            gTimer.Elapsed += new ElapsedEventHandler(Run_Tester);
+        }
+
+        private void Run_Tester(object sender, EventArgs e)
+        {
+            wb.Navigate(new Uri(path.Text));
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            wb.Navigate(new Uri(path.Text));
+            if (gTimer.Enabled == true)
+            {
+                gTimer.Enabled = false;
+            }
+            else
+            {
+                gTimer.Enabled = true;
+            }
         }
 
     }
